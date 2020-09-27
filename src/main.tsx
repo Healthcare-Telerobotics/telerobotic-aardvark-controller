65744b6eb4f598400cdafac62b23873a03fb65ed
Ryan James <ryan@plutovr.com>
2020-09-27 11:14:20 -0700

    Add buttons panel

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
-import { AvGadget, AvPanel, AvStandardGrabbable, AvTransform, HighlightType, DefaultLanding } from '@aardvarkxr/aardvark-react';
-import { EAction, EHand, g_builtinModelBox, InitialInterfaceLock, Av } from '@aardvarkxr/aardvark-shared';
+import {AvStandardGrabbable, AvTransform, DefaultLanding, AvPanel } from '@aardvarkxr/aardvark-react';
+import {Av, g_builtinModelBox } from '@aardvarkxr/aardvark-shared';
 import bind from 'bind-decorator';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
@@ -14,6 +14,16 @@ const tna = new TeleroboticsNetworkAdapter(socket)
 const documentElement = (document as any) as HTMLElement
 const robotController = new RobotController(tna, documentElement)
 
+interface DefaulGadgetState
+{
+	up: boolean
+	down: boolean
+	right: boolean
+	left: boolean
+	deflectUp: boolean
+	deflectDown: boolean
+}
+
 // var velocity = 0
 // setInterval(() => {
 
@@ -21,31 +31,165 @@ const robotController = new RobotController(tna, documentElement)
 // 	robotController.sendFrameData()
 // }, 1000)
 
-const k_TestPanelInterface = "test_panel_counter@1";
 
-interface TestPanelState
-{
-	count: number;
-	grabbableHighlight: HighlightType;
-}
-
-class MyGadget extends React.Component< {}, TestPanelState >
+class MyGadget extends React.Component< {}, DefaulGadgetState >
 {
 	private m_grabbableRef = React.createRef<AvStandardGrabbable>();
 
 	constructor( props: any )
 	{
 		super( props );
+
+		this.state = 
+		{ 
+			up: false,
+			down: false,
+			right: false,
+			left: false,
+			deflectUp: false,
+			deflectDown: false,
+		};
+	}
+
+	@bind
+	private onUp()
+	{
+		this.setState(() => {
+			return {
+				up: true,
+			}
+		})
+	}
+
+	@bind
+	private onUpReleased()
+	{
+		this.setState(() => {
+			return {
+				up: false,
+			}
+		})
+	}
+
+	@bind
+	private onDown()
+	{
+		this.setState(() => {
+			return {
+				down: true,
+			}
+		})
+	}
+
+	@bind
+	private onDownReleased()
+	{
+		this.setState(() => {
+			return {
+				down: false,
+			}
+		})
+	}
+
+	@bind
+	private onLeft()
+	{
+		this.setState(() => {
+			return {
+				left: true,
+			}
+		})
+	}
+
+	@bind
+	private onLeftReleased()
+	{
+		this.setState(() => {
+			return {
+				left: false,
+			}
+		})
+	}
+
+	@bind
+	private onRight()
+	{
+		this.setState(() => {
+			return {
+				right: true,
+			}
+		})
+	}
+
+	@bind
+	private onRightReleased()
+	{
+		this.setState(() => {
+			return {
+				right: false,
+			}
+		})
+	}
+
+	@bind
+	private onDeflectUp()
+	{
+		this.setState(() => {
+			return {
+				deflectUp: true,
+			}
+		})
+	}
+
+	@bind
+	private onDeflectUpReleased()
+	{
+		this.setState(() => {
+			return {
+				deflectUp: false,
+			}
+		})
+	}
+
+	@bind
+	private onDeflectDown()
+	{
+		this.setState(() => {
+			return {
+				deflectDown: true,
+			}
+		})
+	}
+
+	@bind
+	private onDeflectDownReleased()
+	{
+		this.setState(() => {
+			return {
+				deflectDown: false,
+			}
+		})
 	}
 
 	public render()
 	{
+		const controllerString = JSON.stringify(this.state)
 		return (
 			<div>
 				<AvStandardGrabbable modelUri={ g_builtinModelBox } modelScale={ 0.03 }
 						modelColor="lightblue" useInitialParent={ true } ref={ this.m_grabbableRef } remoteInterfaceLocks={[]}>
 					<AvTransform translateY={ 0.08 } >
-						<AvPanel interactive={true} widthInMeters={ 0.1 }/>
+						<AvPanel interactive={true} widthInMeters={ 0.1 }>
+						<div className="controllerValues">
+							{controllerString}
+						</div>
+						<button onMouseDown={this.onUp} onMouseUp={this.onUpReleased}>Up</button>
+						<button onMouseDown={this.onDown} onMouseUp={this.onDownReleased}>Down</button>
+						<button onMouseDown={this.onLeft} onMouseUp={this.onLeftReleased}>Left</button>
+						<button onMouseDown={this.onRight} onMouseUp={this.onRightReleased}>Right</button>
+						<button onMouseDown={this.onDeflectUp} onMouseUp={this.onDeflectUpReleased}>Deflect Up</button>
+						<button onMouseDown={this.onDeflectDown} onMouseUp={this.onDeflectDownReleased}>Deflect Down</button>
+						</AvPanel>
 					</AvTransform>
 				</AvStandardGrabbable>
 			</div>
